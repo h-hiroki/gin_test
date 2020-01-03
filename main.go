bafd834b15571206864b143102add5e2efe7c3d2
hhiroki <gyouzakuuka@gmail.com>
2020-01-04 00:21:34 +0900

    get image file

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
 	engine := gin.Default()
-	engine.Static("/static", "./static")
+	engine.POST("/upload", func(c *gin.Context) {
+		file, handler, err := c.Request.FormFile("image")
+		if err != nil {
+			c.String(http.StatusBadRequest, "Bad Request")
+			return
+		}
+
+		fileName := handler.Filename
+		dir, _ := os.Getwd()
+		out, err := os.Create(dir + "\\images\\" + fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+
+		_, err = io.Copy(out, file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
 	engine.Run(":3000")
 }
